ee9b262986a34cc8b09713a1b3e72fb59e95fccb
Krunal Gandhi <krunal1313@gmail.com>
2018-03-16 17:08:38 +0530

    clean up

--- kernel.cu
+++ kernel.cu
@@ -3,9 +3,8 @@
 
 int main()
 {
-    //char ch;
-	clock_t beg,end;
-	beg=clock();
+    clock_t beg,end;
+    beg=clock();
     int source = 0;
     int dest = 0;
     cudaError_t err = cudaSuccess;
@@ -31,37 +30,24 @@ int main()
 
     float *d_Graph = NULL;
     
-	cudaMalloc((void **)&d_Graph, size * numberOfVertex );
+    cudaMalloc((void **)&d_Graph, size * numberOfVertex );
     if (d_Graph == NULL)
     {
         cout <<"Failed" << endl;
     }
-
-    //thread number
-/*
-    int threadsPerBlock = numberOfVertex ;
-    int blocksPerGrid =(numberOfVertex  + threadsPerBlock - 1) / threadsPerBlock;
-	*/
+	
     //Read Graph file.
-
     fstream fp("f1.txt",ios::in);
     if(!fp.is_open())
     {
         printf("Failed to open file.\n");
     }
 
-    //output file
-  /*  fstream prFile("PageRankValue.txt", ios::out);
-    if (!prFile.is_open())
-    {
-        printf("Failed to open file PRV\n");
-    }
-	*/
     //host memory allocate
 
-	float *Grap=new float[numberOfVertex*numberOfVertex ];
+    float *Grap=new float[numberOfVertex*numberOfVertex ];
 	
-	float *PR=new float[numberOfVertex ];
+    float *PR=new float[numberOfVertex ];
     float *PR_Temp=new float[numberOfVertex];
 
 
@@ -79,20 +65,18 @@ int main()
             Grap[i*numberOfVertex+j] = 0;
         }
     }
-	int edge = 0;
+
+    int edge = 0;
+	
     //read from Graph.txt
-    while (!fp.eof()){
-
+    while (!fp.eof())
+    {
         fp >> source >> dest;
-     //   std::cout << source << ' '<< dest << std::endl;
-
         Grap[(source-1)*numberOfVertex+(dest-1)] = 1;
-		Grap[(dest-1)*numberOfVertex+(source-1)] = 1;
-		edge++;
+	Grap[(dest-1)*numberOfVertex+(source-1)] = 1;
+	edge++;
     }
     printf("Graph build Done!\n");
-//    printf("----------------------------------------------------------\n");
-
 
     //copy
     err = cudaMemcpy(d_Graph, Grap, numberOfVertex *size, cudaMemcpyHostToDevice);
@@ -104,33 +88,15 @@ int main()
     }
 
     //invoke PageRank.
-	//CPU Routine
- 
-  //  int CPUTime  = 0;
-//    CPUTime = PageRank(Graph, PR);
-
-
-	for (int i = 0; i < numberOfVertex; ++i)
+    //CPU Routine
+    for (int i = 0; i < numberOfVertex; ++i)
     {
         PR[i] = InitPageRankValue;
     }
-   // printf("--------------------------------------------------------\n");
-  //  clock_t begin, end;
+
     int iter = 0;
-//    float SumOfGPUTime = 0;
-    //begin = clock();
     for (int m = 0; m < Max_Iteration_Number; ++m)
     {
-        /*
-        for (int i = 0; i < numberOfVertex ; ++i)
-        {
-            for (int j = 0; j < numberOfVertex ; ++j)
-            {
-                printf("%f\t", Graph[i][j]);
-            }
-            printf("\n");
-        }
-        */
 
         iter ++;
 
@@ -144,8 +110,7 @@ int main()
         
         //calculate sum of out degree
 
-        //claculateSumOfOutDegree<<<blocksPerGrid, threadsPerBlock>>>(d_Sum_Of_Degree, d_Graph);
-		claculateSumOfOutDegree<<<256,256>>>(d_Sum_Of_Degree, d_Graph);
+	claculateSumOfOutDegree<<<256,256>>>(d_Sum_Of_Degree, d_Graph);
         err = cudaGetLastError();
 
         if (err != cudaSuccess)
@@ -153,13 +118,6 @@ int main()
             fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n", cudaGetErrorString(err));
             exit(EXIT_FAILURE);
         }
-        /*
-        cudaMemcpy(sumOfOutDegree, d_Sum_Of_Degree, size, cudaMemcpyDeviceToHost);
-        for (int i = 0; i < numberOfVertex ; ++i)
-        {
-            cout << sumOfOutDegree[i] <<'\t';
-        }
-        */
 
         //copy
         err = cudaMemcpy(d_PR, PR, size, cudaMemcpyHostToDevice);
@@ -170,8 +128,8 @@ int main()
             exit(EXIT_FAILURE);
         }
 
-        //PRAdd<<<blocksPerGrid, threadsPerBlock>>>(d_PR, d_Graph, d_Sum_Of_Degree);
-		PRAdd<<<256,256>>>(d_PR, d_Graph, d_Sum_Of_Degree);
+	// add the sum
+	PRAdd<<<256,256>>>(d_PR, d_Graph, d_Sum_Of_Degree);
         err = cudaGetLastError();
 
         if (err != cudaSuccess)
@@ -184,162 +142,104 @@ int main()
         float elapsedTime;
         cudaEventElapsedTime(&elapsedTime, start, stop);
 
-     //   SumOfGPUTime += elapsedTime;
-
         cudaMemcpy(PR_Temp, d_PR, size, cudaMemcpyDeviceToHost);
 
        if (END(PR_Temp, PR))
         {
             break;
         }
-        else{
+        else
+	{
             for (int i = 0; i < numberOfVertex ; ++i)
             {
                 PR[i] = PR_Temp[i];
             }
-        }
-        
-		
+        }	
     }
-	cudaMemcpy(d_PR, PR, size, cudaMemcpyHostToDevice);
-    //end = clock();
-    
 
+    cudaMemcpy(d_PR, PR, size, cudaMemcpyHostToDevice);    
 
-    //printf("%d\n", vertex);
-    
-  /*  for (int i = 0; i < numberOfVertex; ++i)
-    {
-        prFile << i <<" "<< PR[i] << endl;
-    }
-    */
-
-    
-
-	//cout<<PR[9204];
     fp.close();
-	string ip,tok;
-	fp.open("genes");
-	map<string,int>gen;
-	while(getline(fp,ip))
+    string ip,tok;
+    fp.open("genes");
+    map<string,int>gen;
+    
+    while(getline(fp,ip))
+    {
+	istringstream iss(ip);
+	iss>>source>>tok;
+	gen.insert(pair<string,int>(tok,source-1));
+    }
+	
+   fp.close();
+   fp.open("da.tsv");
+   map<string,int>::iterator it;
+	
+
+   vector<int> v;
+   long long t=0;
+   while(getline(fp,ip))
+   {
+	t++;
+	istringstream iss(ip);
+	iss>>tok;
+	it=gen.find(tok);
+	if(it!=gen.end())
 	{
-		istringstream iss(ip);
-		iss>>source>>tok;
-		gen.insert(pair<string,int>(tok,source-1));
-	}
-	
-	fp.close();
-	fp.open("da.tsv");
-	map<string,int>::iterator it;
-	
+		v.push_back(gen.find(tok)->second);
+	}	
+   }
 
-	vector<int>v;
-	long long t=0;
-	while(getline(fp,ip))
+   printf("\n size:%lld",t);
+   int *va = new int[v.size()];
+   copy(v.begin(),v.end(),va);
+	
+   int *dva=0;
+   cudaMalloc((void **)&dva,sizeof(int)*v.size());
+   err = cudaMemcpy(dva, va, sizeof(int)*v.size(), cudaMemcpyHostToDevice);
+
+   if (err != cudaSuccess)
+   {
+   	fprintf(stderr, "Failed to copy vector B from host to device (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    int *dfreq=0;
+    float *dans=0;
+    cudaMalloc((void **)&dfreq,sizeof(int)*numberOfVertex);
+    cudaMalloc((void **)&dans,sizeof(float)*numberOfVertex);
+		
+    calcfreq<<<256,256>>>(dva,dfreq,v.size());
+		
+    mul<<<256,256>>>(dans,dfreq,d_PR);
+		
+   thresh<<<256,256>>>(d_Graph,dans,100);
+
+		
+
+   err = cudaMemcpy(Grap, d_Graph, numberOfVertex *size,cudaMemcpyDeviceToHost);
+
+   if (err != cudaSuccess)
+   {
+   	fprintf(stderr, "Failed to copy vector B from host to device (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+   }
+
+   freopen("subnets2.txt","w",stdout);
+   Graph g(numberOfVertex);
+   
+   for(int i=0;i<numberOfVertex;i++)
+   {
+	for(int j=0;j<numberOfVertex;j++)
 	{
-		t++;
-		istringstream iss(ip);
-		iss>>tok;
-		it=gen.find(tok);
-		if(it!=gen.end())
-		{
-			//freq[gen.find(tok)->second]++;
-			v.push_back(gen.find(tok)->second);
-		}	
-	}
-	printf("\n size:%lld",t);
-    int *va=new int[v.size()];
-	copy(v.begin(),v.end(),va);
+		if(Grap[i*numberOfVertex+j]==1)
+		g.addEdge(i,j);
+	}			
+   }	
 	
-	int *dva=0;
-	cudaMalloc((void **)&dva,sizeof(int)*v.size());
-	err = cudaMemcpy(dva, va, sizeof(int)*v.size(), cudaMemcpyHostToDevice);
-
-        if (err != cudaSuccess)
-        {
-            fprintf(stderr, "Failed to copy vector B from host to device (error code %s)!\n", cudaGetErrorString(err));
-            exit(EXIT_FAILURE);
-        }
-
-		int *dfreq=0;
-		float *dans=0;
-		cudaMalloc((void **)&dfreq,sizeof(int)*numberOfVertex);
-		cudaMalloc((void **)&dans,sizeof(float)*numberOfVertex);
-		
-		calcfreq<<<256,256>>>(dva,dfreq,v.size());
-		
-		mul<<<256,256>>>(dans,dfreq,d_PR);
-		
-	/*	float *ans=new float[numberOfVertex];
-
-		err = cudaMemcpy(ans, dans, size,cudaMemcpyDeviceToHost);
-		ofstream l;
-		l.open("final.txt");
-		for(int i=0;i<numberOfVertex;i++)
-		{
-			l<<ans[i]<<endl;
-		}
-		l.close();
-		*/
-		thresh<<<256,256>>>(d_Graph,dans,100);
-
-		
-
-		err = cudaMemcpy(Grap, d_Graph, numberOfVertex *size,cudaMemcpyDeviceToHost);
-
-        if (err != cudaSuccess)
-        {
-            fprintf(stderr, "Failed to copy vector B from host to device (error code %s)!\n", cudaGetErrorString(err));
-            exit(EXIT_FAILURE);
-        }
-
-		
-/*		int cnt;
-		ofstream op;
-		op.open("result.txt");
-		vector<int>vi;
-		for(int i=0;i<numberOfVertex;i++)
-		{
-			cnt=0;
-			vi.clear();
-			
-			for(int j=0;j<numberOfVertex;j++)
-			{
-				if(Grap[i*numberOfVertex+j]==1)
-				{
-					cnt++;
-					vi.push_back(j);
-				}
-				
-			}
-			if(cnt>=3)
-			{
-				op<<i<<" : ";
-				for(int k=0;k<vi.size();k++)
-				op<<vi[k]<<" ";
-				
-				op<<endl;
-			}
-		 } 
-*/
-
-			freopen("subnets2.txt","w",stdout);
-		Graph g(numberOfVertex);
-		for(int i=0;i<numberOfVertex;i++)
-			{
-				for(int j=0;j<numberOfVertex;j++)
-				{
-					if(Grap[i*numberOfVertex+j]==1)
-						g.addEdge(i,j);
-					
-				}			
-			}	
-			g.printSCCs();
+  g.printSCCs();
 				
 
-		end=clock();
-		printf("\n Time taken is : %.9f",(double)(end-beg)/CLOCKS_PER_SEC);
-
-
-		getchar();
+  end=clock();
+  printf("\n Time taken is : %.9f",(double)(end-beg)/CLOCKS_PER_SEC);
 }
